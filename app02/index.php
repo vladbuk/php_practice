868ee44f4f122f0aa982fa34306d4934b544d8a9
vladbuk <vladbuk@gmail.com>
2017-12-21 20:56:56 +0200

    app2 added

---
+++ app02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>app2:: grid css</title>
+</head>
+<body>
+    <div id="div1">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+</body>
+</html>
